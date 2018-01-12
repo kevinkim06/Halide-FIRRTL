025eee2ae4c0e9d4671167561bbdb1fbfa2084ae
Taesu Kim <tae.s.kim@intel.com>
2018-01-11 23:19:00 -0800

    Adding -ljpeg

--- apps/hls_examples/hls_support/run_hls.tcl
+++ apps/hls_examples/hls_support/run_hls.tcl
@@ -1,7 +1,7 @@
 set HALIDE_PATH $env(RUN_PATH)/../../..
 set HLS_INC_FLAGS "-I${HALIDE_PATH}/include -I$env(RUN_PATH)/../hls_support"
 set INC_FLAGS "-I${HALIDE_PATH}/include -I${HALIDE_PATH}/tools -I$env(RUN_PATH)/../../support -I$env(RUN_PATH)/../hls_support"
-set LD_FLAGS "$env(RUN_PATH)/pipeline_native.o -lpthread -ldl -lpng12"
+set LD_FLAGS "$env(RUN_PATH)/pipeline_native.o -lpthread -ldl -lpng12 -ljpeg"
 
 # creating the project and seting up the environtment
 open_project hls_prj
