44233ba8b5bd9b515569ff6147b021746d334a4a
Taesu Kim <tae.s.kim@intel.com>
2018-02-28 17:09:27 -0800

    adding 3D linebuffer case dim2 is c and extent is 1

---
+++ apps/firrtl_examples/blur_linebuffer3D1_hls/axi_config.v
@@ -0,0 +1,129 @@
+`timescale 1ns/1ps
+
+module axi_config(
+    input        clk,
+    input        reset,
+    output reg [31:0] ARADDR,
+    output reg  ARVALID,
+    output reg [31:0] AWADDR,
+    output reg  AWVALID,
+    output reg  BREADY,
+    output reg  RREADY,
+    output reg [31:0] WDATA,
+    output reg [3:0] WSTRB,
+    output reg  WVALID,
+    input ARREADY,
+    input AWREADY,
+    input [1:0] BRESP,
+    input BVALID,
+    input [31:0] RDATA,
+    input [1:0] RRESP,
+    input RVALID,
+    input WREADY,
+    input start,
+    output reg done,
+    output reg stop_sim
+);
+
+reg [31:0] r_data;
+
+localparam ADDR_CTRL = 0;
+localparam ADDR_STATUS = 4;
+localparam ADDR_ENABLE = 'h40;
+localparam ADDR_WEIGHT = 'h44;
+
+task axi_read(
+    input [31:0] addr,
+    output[31:0] data
+);
+begin
+    ARADDR = addr;
+    ARVALID = 1;
+    while(ARREADY==0) @(posedge clk);
+    @(posedge clk);
+    ARVALID = 0;
+    RREADY = 1;
+    while(RVALID==0) @(posedge clk);
+    @(posedge clk);
+    data = RDATA;
+end
+endtask
+
+task axi_write(
+    input [31:0] addr,
+    input [31:0] data
+);
+begin
+    BREADY = 0;
+    AWADDR = addr;
+    AWVALID = 1;
+    WDATA = data;
+    WVALID = 1;
+    WSTRB = 15; // TODO
+    while(AWREADY==0) @(posedge clk);
+    @(posedge clk);
+    AWVALID = 0;
+    while(WREADY==0) @(posedge clk);
+    @(posedge clk);
+    WVALID = 0;
+    BREADY = 1;
+    while(BVALID==0) @(posedge clk);
+    @(posedge clk);
+    BREADY = 0;
+end
+endtask
+
+initial begin
+    ARADDR = 0;
+    ARVALID = 0;
+    AWADDR = 0;
+    AWVALID = 0;
+    BREADY = 0;
+    RREADY = 0;
+    WDATA = 0;
+    WSTRB = 0;
+    WVALID = 0;
+    done = 0;
+    stop_sim = 0;
+    @(start);
+    axi_write (ADDR_ENABLE, 1);
+    axi_write (ADDR_WEIGHT, 1);
+    axi_write (ADDR_WEIGHT+4, 3);
+    axi_write (ADDR_WEIGHT+8, 3);
+    axi_write (ADDR_WEIGHT+12, 15);
+    axi_write (ADDR_WEIGHT+16, 6);
+    axi_write (ADDR_WEIGHT+20, 25);
+    axi_write (ADDR_WEIGHT+24, 3);
+    axi_write (ADDR_WEIGHT+28, 15);
+    axi_write (ADDR_WEIGHT+32, 1);
+    axi_write (ADDR_WEIGHT+36, 3);
+    axi_read (ADDR_ENABLE, r_data);
+    axi_read (ADDR_WEIGHT, r_data);
+    axi_read (ADDR_WEIGHT+4, r_data);
+    axi_read (ADDR_WEIGHT+8, r_data);
+    axi_read (ADDR_WEIGHT+12, r_data);
+    axi_read (ADDR_WEIGHT+16, r_data);
+    axi_read (ADDR_WEIGHT+20, r_data);
+    axi_read (ADDR_WEIGHT+24, r_data);
+    axi_read (ADDR_WEIGHT+28, r_data);
+    axi_read (ADDR_WEIGHT+32, r_data);
+    axi_read (ADDR_WEIGHT+36, r_data);
+    axi_write (ADDR_CTRL, 1);
+    done = 1;
+    while (1) begin
+        axi_read(ADDR_CTRL, r_data);
+        #100;
+        @(posedge clk);
+        if (r_data==2) begin
+            axi_write (ADDR_CTRL, 2);
+            stop_sim = 1;
+        end
+        #100;
+        @(posedge clk);
+        axi_read(ADDR_STATUS, r_data);
+        #100;
+        @(posedge clk);
+    end
+end
+
+endmodule
---
+++ apps/firrtl_examples/blur_linebuffer3D1_hls/pipeline.cpp
@@ -0,0 +1,107 @@
+#include "Halide.h"
+
+using namespace Halide;
+
+Var x("x"), y("y"), c("c");
+Var xo("xo"), xi("xi"), yi("yi"), yo("yo");
+
+class MyPipeline {
+    ImageParam input;
+    ImageParam weight;
+    Param<bool> enable;
+    Func in;
+    Func blur_x, blur_y;
+    Func hw_output;
+    Func output;
+    std::vector<Argument> args;
+
+public:
+    MyPipeline() : input(UInt(8), 3, "input"),
+                   weight(UInt(8), 2, "weight"),
+                   in("in"),
+                   blur_x("blur_x"),
+                   blur_y("blur_y"),
+                   hw_output("hw_output"),
+                   output("output") {
+        in(x, y, c) = input(x, y, c);
+        blur_x(x, y, c) = (in(x, y, c) * weight(0,0)
+                        +  in(x+1, y, c) * weight(0,1)
+                        +  in(x+2, y, c) * weight(0,2)
+                        +  in(x+3, y, c) * weight(0,3)
+                        +  in(x+4, y, c) * weight(0,4))/5;
+        blur_y(x, y, c) = (blur_x(x, y, c) * weight(1,0)
+                        +  blur_x(x, y+1, c) * weight(1,1)
+                        +  blur_x(x, y+2, c) * weight(1,2))/3;
+        hw_output(x, y, c) = select(enable==true, blur_y(x, y, c), in(x, y, c));
+        output(x, y, c) = hw_output(x, y, c);
+
+        weight.dim(0).set_bounds(0, 2);
+        weight.dim(1).set_bounds(0, 5);
+        weight.dim(0).set_stride(1);
+        weight.dim(1).set_stride(2);
+
+        args = {input, enable, weight};
+    }
+
+
+    void compile_cpu() {
+        std::cout << "\ncompiling cpu code..." << std::endl;
+
+        output.tile(x, y, xo, yo, xi, yi, 256, 256);//.reorder(c, xi, yi, xo, yo);
+        output.bound(x, 0, 256).bound(y, 0, 256).bound(c, 0, 1);
+
+        output.compile_to_header("pipeline_native.h", args, "pipeline_native");
+        output.compile_to_object("pipeline_native.o", args, "pipeline_native");
+
+        std::vector<Target::Feature> features({Target::Zynq});
+        Target target(Target::Linux, Target::ARM, 32, features);
+        output.compile_to_header("pipeline_arm.h", args, "pipeline_native", target);
+        output.compile_to_object("pipeline_arm.o", args, "pipeline_native", target);
+    }
+
+    void compile_hls() {
+        std::cout << "\ncompiling HLS and FIRRTL code..." << std::endl;
+
+        in.compute_at(output, xo);
+
+        blur_x.compute_at(output, xo);
+        blur_x.linebuffer();
+        in.fifo_depth(hw_output, 256*2+5+5+2000);
+        //blur_x.unroll(c);
+
+        hw_output.accelerate({in}, xi, xo);
+        hw_output.compute_at(output, xo);
+        hw_output.tile(x, y, xo, yo, xi, yi, 256, 256).reorder(c, xi, yi, xo, yo);
+        hw_output.bound(x, 0, 256).bound(y, 0, 256).bound(c, 0, 1);
+        //hw_output.unroll(c);
+
+        output.tile(x, y, xo, yo, xi, yi, 256, 256).reorder(c, xi, yi, xo, yo);
+        output.bound(x, 0, 256).bound(y, 0, 256).bound(c, 0, 1);
+        output.unroll(c);
+
+        Target hls_target = get_target_from_environment();
+        hls_target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_hls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target);
+
+        std::vector<Target::Feature> features({Target::Zynq});
+        Target target(Target::Linux, Target::ARM, 32, features);
+        target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_zynq_c("pipeline_zynq.cpp", args, "pipeline_hls", target);
+        output.compile_to_header("pipeline_zynq.h", args, "pipeline_hls", target);
+
+        // FIRRTL generation
+        output.compile_to_firrtl("pipeline_firrtl.cpp", args, "pipeline_firrtl", hls_target);
+    }
+};
+
+
+int main(int argc, char **argv) {
+    MyPipeline p1;
+    p1.compile_cpu();
+
+    MyPipeline p2;
+    p2.compile_hls();
+
+    return 0;
+}
